c6b3cd120ab56f297a28bc534da945f0d19a298e
mettetal-charlotte <charlotte.mettetal@edu.univ-fcomte.fr>
2026-03-12 11:44:27 +0100

    artistes

--- backend/backend.mjs
+++ backend/backend.mjs
@@ -70,3 +70,23 @@ export async function updateArtiste(id, updatedArtiste) {
     const record = await pb.collection('Artiste').update(id, updatedArtiste);
     return record;
 }
+
+const options = {
+    day: "numeric",
+    month: "long",
+    year: "numeric",
+    //hour: "numeric",
+    //minute: "numeric",
+    //hour12: false,
+    timeZone: "Europe/Paris",
+};
+
+const formatter = new Intl.DateTimeFormat("fr-FR", options);
+
+export function formatDate(dateString) {
+    const date = new Date(dateString);
+
+    const formattedDate = formatter.format(date);
+
+    return formattedDate;
+}
